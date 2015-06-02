e4f89bebbc02a39160591e5d1dbdd559c633e604
Scott A Thisse <scottt@bandito-gw.(none)>
2015-06-02 18:59:40 -0400

    Add graphic card images

--- app/src/main/java/org/tbadg/memory/Card.java
+++ app/src/main/java/org/tbadg/memory/Card.java
@@ -1,16 +1,28 @@
 package org.tbadg.memory;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class Card extends Button {
 
     private Integer mValue;
+    private int images[] = new int[MemoryActivity.MAX_MATCHES];
 
     public Card(Context context) {
         super(context);
+//        this.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+        setup(context);
     }
+
+    public void setup(Context context) {
+        for (int x = 0; x < images.length; x++) {
+            images[x] = getResources().getIdentifier("@drawable/card_" + String.valueOf(x), null,
+                                                     context.getPackageName());
+        }
+    }
+
 
     public void setValue(Integer value) {
         mValue = value;
@@ -30,11 +42,14 @@ public class Card extends Button {
     public void showBack() {
         setVisibility(View.VISIBLE);
         setText("");
+        setBackgroundResource(R.drawable.card_back);
     }
 
     public void showFront() {
         setVisibility(View.VISIBLE);
         setText(String.valueOf(mValue));
+        Log.e("Card", "Resource ID = " + mValue);
+        setBackgroundResource(images[mValue]);
     }
 
     public boolean equals(Card other) {
